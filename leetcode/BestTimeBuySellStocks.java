f116a34969ed954b9c5f88069e1caa76fbdf4578
Gaurav Pratap Singh <nikumbgo@users.noreply.github.com>
2022-07-10 19:00:50 +0530

    BestbuySell
    BestbuySell

---
+++ leetcode/BestTimeBuySellStocks.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * <p>
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * <p>
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ */
+public class BestTimeBuySellStocks {
+
+    public static void main(String args[]) {
+        System.out.println(maxProfit(new int[]{1, 6, 9, 66, 88, 99}));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int left = Integer.MAX_VALUE, right = 0, temp = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < left) {
+                left = prices[i];
+                for (int j = i + 1; j < prices.length; j++) {
+                    right = prices[j];
+                    if (right > left && (right - left) > temp)
+                        temp = right - left;
+                }
+            }
+        }
+        return temp;
+    }
+}
