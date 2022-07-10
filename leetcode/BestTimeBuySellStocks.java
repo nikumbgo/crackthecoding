3247f193ac45a02ead2ce620519f431a368a4685
Gaurav Pratap Singh <nikumbgo@users.noreply.github.com>
2022-07-10 19:07:55 +0530

    Update BestTimeBuySellStocks.java

--- leetcode/BestTimeBuySellStocks.java
+++ leetcode/BestTimeBuySellStocks.java
@@ -39,3 +39,19 @@ public class BestTimeBuySellStocks {
         return temp;
     }
 }
+
+/**
+ * public class Solution {
+ *     public int maxProfit(int prices[]) {
+ *         int minprice = Integer.MAX_VALUE;
+ *         int maxprofit = 0;
+ *         for (int i = 0; i < prices.length; i++) {
+ *             if (prices[i] < minprice)
+ *                 minprice = prices[i];
+ *             else if (prices[i] - minprice > maxprofit)
+ *                 maxprofit = prices[i] - minprice;
+ *         }
+ *         return maxprofit;
+ *     }
+ * }
+ */
